863bf9f60c6af9a373e1d06c6aa8bb57f5827ba5
Mark Primak <markprimak802@gmail.com>
2025-07-04 16:06:27 -0400

    Un-f*cking the repo

--- src/test/java/com/example/starter/StarterIT.java
+++ src/test/java/com/example/starter/StarterIT.java
@@ -23,7 +23,7 @@ import com.flowlogix.util.ShrinkWrapManipulator;
 import org.eu.ingwar.tools.arquillian.extension.suite.annotations.ArquillianSuiteDeployment;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit5.ArquillianExtension;
-import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -55,7 +55,7 @@ class StarterIT {
 
     @Deployment
     @SuppressWarnings("unused")
-    static JavaArchive deploy() {
-        return ShrinkWrapManipulator.createDeployment(JavaArchive.class);
+    static WebArchive deploy() {
+        return ShrinkWrapManipulator.createDeployment(WebArchive.class);
     }
 }
