36e1a2645e5535e0e7aa76dcba32372775efbd11
Lenny Primak <lenny@flowlogix.com>
2025-07-05 14:54:54 -0500

    enh: cleaned up starter test (#7)
    * enh: cleaned up starter test
    
    * enh: removed multiplication

--- src/test/java/com/example/starter/StarterTest.java
+++ src/test/java/com/example/starter/StarterTest.java
@@ -2,22 +2,23 @@ package com.example.starter;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Random;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class StarterTest {
     @Test
     @SuppressWarnings("checkstyle:MagicNumber")
     void calcBasicFunc() {
-        int a = (int) (Math.random() * 100) + 1;
-        int b = (int) (Math.random() * 100) + 1;
+        var random = new Random();
+        int a = random.nextInt(1, 1000);
+        int b = random.nextInt(1, 1000);
         Calculator calculator = new Calculator();
         assertThat(calculator.add(a, b)).isEqualTo(a + b);
         assertThat(calculator.subtract(a, b)).isEqualTo(a - b);
         assertThat(calculator.multiply(a, b)).isEqualTo(a * b);
         assertThat(calculator.divide(a, b)).isEqualTo(a / b);
-        assertThatThrownBy(() -> calculator.divide(a, 0))
-            .isInstanceOf(IllegalArgumentException.class)
-            .hasMessage("Division by zero is not allowed.");
+        assertThatIllegalArgumentException().isThrownBy(() -> calculator.divide(a, 0))
+            .withMessage("Division by zero is not allowed.");
     }
 }
