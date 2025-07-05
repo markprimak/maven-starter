531df4d9d6bc774a5a0ede16866c195f18f0b855
Lenny Primak <lenny@flowlogix.com>
2025-07-05 14:49:56 -0500

    enh: move and add IT (#9)
    * enh: move and add IT
    
    * chore(jacoco): narrow down jacoco-covered packages to starter
    
    * bugfix(jacoco): moved jacoco exclusion path to one directoy up (example)
    
    * enh: better integration test for subtract()
    
    * Suppress magic number for IT test

--- src/test/java/com/example/starter/its/StarterIT.java
+++ src/test/java/com/example/starter/its/StarterIT.java
@@ -16,25 +16,35 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.example.starter;
+package com.example.starter.its;
 
+import com.example.starter.Calculator;
 import com.flowlogix.testcontainers.PayaraServerLifecycleExtension;
 import com.flowlogix.util.ShrinkWrapManipulator;
+import jakarta.inject.Inject;
 import org.eu.ingwar.tools.arquillian.extension.suite.annotations.ArquillianSuiteDeployment;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit5.ArquillianExtension;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import java.util.Random;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(PayaraServerLifecycleExtension.class)
 @ExtendWith(ArquillianExtension.class)
 @ArquillianSuiteDeployment
 class StarterIT {
+    @Inject
+    Calculator calculator;
+
     @Test
-    void sanityCheck() {
-        assertThat(true).isTrue();
+    @SuppressWarnings("checkstyle:MagicNumber")
+    void subtract() {
+        var random = new Random();
+        int a = random.nextInt(1, 1000);
+        int b = random.nextInt(1, 1000);
+        assertThat(calculator.subtract(a, b)).isEqualTo(a - b);
     }
 
     @Deployment
