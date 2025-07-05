93a05a11a22a51685a97217587745b2e27c4e6fd
Mark Primak <mprimak@flowlogix.com>
2025-07-05 16:25:59 -0400

    Split up unit test into 4 separate tests

--- src/test/java/com/example/starter/StarterTest.java
+++ src/test/java/com/example/starter/StarterTest.java
@@ -1,24 +1,49 @@
 package com.example.starter;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class StarterTest {
-    @Test
+
+    private final Calculator calculator = new Calculator();
+    private final Random random = new Random();
+
+    private int a;
+    private int b;
+
+    @BeforeEach
     @SuppressWarnings("checkstyle:MagicNumber")
-    void calcBasicFunc() {
-        var random = new Random();
-        int a = random.nextInt(1, 1000);
-        int b = random.nextInt(1, 1000);
-        Calculator calculator = new Calculator();
+    void setUp() {
+        a = random.nextInt(1, 1000);
+        b = random.nextInt(1, 1000);
+    }
+
+    @Test
+    void testAdditionWithRandomInputs() {
         assertThat(calculator.add(a, b)).isEqualTo(a + b);
+    }
+
+    @Test
+    void testSubtractionWithRandomInputs() {
         assertThat(calculator.subtract(a, b)).isEqualTo(a - b);
+    }
+
+    @Test
+    void testMultiplicationWithRandomInputs() {
         assertThat(calculator.multiply(a, b)).isEqualTo(a * b);
+    }
+
+    @Test
+    void testDivisionAndDivisionByZero() {
         assertThat(calculator.divide(a, b)).isEqualTo(a / b);
-        assertThatIllegalArgumentException().isThrownBy(() -> calculator.divide(a, 0))
+
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> calculator.divide(a, 0))
             .withMessage("Division by zero is not allowed.");
     }
 }
