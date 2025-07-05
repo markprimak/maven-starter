cc0217991434bc03a4f74c6c1685b9d6358db74e
lprimak <lenny@flowlogix.com>
2025-07-04 21:33:00 -0500

    enh: move and add IT

--- src/main/java/com/example/starter/Calculator.java
+++ src/main/java/com/example/starter/Calculator.java
@@ -1,20 +1,26 @@
 package com.example.starter;
 
+import jakarta.enterprise.context.Dependent;
+
 /**
  * A simple calculator class that provides basic arithmetic operations.
  * This class can be used to perform addition, subtraction, multiplication,
  * and division of two integers.
  */
+@Dependent
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
     }
+
     public int subtract(int a, int b) {
         return a - b;
     }
+
     public int multiply(int a, int b) {
         return a * b;
     }
+
     public int divide(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("Division by zero is not allowed.");
