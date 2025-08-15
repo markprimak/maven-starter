432ca6f362dbd3f5535de4e66601a644dcfe3c82
markp103 <mark.primak55@myhunter.cuny.edu>
2025-08-15 20:01:47 +0000

    Fixed checkstyle unused import

--- src/test/java/com/example/starter/its/IndexPageIT.java
+++ src/test/java/com/example/starter/its/IndexPageIT.java
@@ -31,7 +31,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.PageFactory;
 import org.jboss.arquillian.graphene.page.Location;
 import org.jboss.arquillian.graphene.page.Page;
 import org.jboss.arquillian.graphene.Graphene;
